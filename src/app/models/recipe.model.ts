4f791b4b3f21d1d3583bbaed5824f835f1d96a89
nicolerchoi <84108293+nicolerchoi@users.noreply.github.com>
2026-05-24 03:22:06 +0100

    App set up (#1)
    * Feat: Initialize base Angular project scaffolding
    
    * supabase setup
    
    * dashboard

---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,27 @@
+export interface Recipe {
+    id: number;
+    title: string;
+    servings_base: number;
+    image_url: string | null;
+    instructions: string[];
+    created_at: string;
+    tags: string[];
+    // Optional: We can nest these when fetching deep relational data
+    ingredient_groups?: IngredientGroup[];
+}
+
+export interface IngredientGroup {
+    id: number;
+    recipe_id: number;
+    name: string;
+    // Optional: Nesting the actual ingredients inside their respective section
+    ingredients?: Ingredient[];
+}
+
+export interface Ingredient {
+    id: number;
+    group_id: number;
+    name: string;
+    base_quantity: number;
+    unit: string;
+}
